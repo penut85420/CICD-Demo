9aeced4c01748bd7211a357507c6093fe17d1d88
PenutChen <penut85420@gmail.com>
2021-05-25 17:57:19 +0800

    First commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from pkg.utils import plus, minus, multi
+
+print(plus(3, 4))
+print(minus(5, 6))
+print(multi(7, 8))
---
+++ pkg/utils.py
@@ -0,0 +1,8 @@
+def plus(a, b):
+    return a + b
+
+def minus(a, b):
+    return a - b
+
+def multi(a, b):
+    return a * b
